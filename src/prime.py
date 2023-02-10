dc1ae6d5850a458950e6f890b4874f3cb473b765
Frans Bol <8735507+nimbol@users.noreply.github.com>
2023-02-10 12:34:42 +0000

    Kata
    Co-authored-by: Gavin Leake <gavlt@users.noreply.github.com>
    Co-authored-by: Ricardo Xavier <pevidex@users.noreply.github.com>
    Co-authored-by: João Miguel <jrsm-github@outlook.com>

--- src/prime.py
+++ src/prime.py
@@ -4,6 +4,7 @@ This is an exercise in optimisation, iteration and problem solving.
 Possible approaches are outlined below.
 """
 from typing import List, Optional, Sequence
+import math
 
 
 def is_prime(n: int, known_primes: Sequence[int]) -> bool:
@@ -18,6 +19,18 @@ def prime_generator() -> Sequence[int]:
     Just start from the beginning and keep yielding calculated primes.
     This approach is simple and may become inefficient for large numbers.
     """
+    yield 2
+    num = 1
+    divisor = 3
+    while True:
+        num += 2
+        divisor = 3
+        while divisor <= math.isqrt(num):
+            if num % divisor == 0:
+                break
+            divisor += 2
+        else:
+            yield num
 
 
 def prime_sieve(limit: int) -> List[int]:
@@ -26,3 +39,24 @@ def prime_sieve(limit: int) -> List[int]:
     using the sieve of Eratosthenes algorithm.
     This approach is pretty efficient, but obviously literally limited.
     """
+    possibilities = list(range(3, limit + 1, 2))
+    idx = 0
+    max_test = math.sqrt(limit + 1)
+    while possibilities[idx] < max_test:
+        p = possibilities[idx]
+        for n in range(p * p, limit + 1, p * 2):
+            try:
+                possibilities.remove(n)
+            except ValueError:
+                pass
+        idx += 1
+    return [2] + possibilities
+
+
+if __name__ == "__main__":
+    print(prime_sieve(100_000)[-1])
+    # g = prime_generator()
+    # for _ in range(100_000):
+    #     next(g)
+    # for _ in range(900_000):
+    #     print(next(g))
--- test/test_prime.py
+++ test/test_prime.py
@@ -1,23 +1,57 @@
+import itertools
+
 import pytest
 
 import prime
 
 
-@pytest.mark.parametrize("n", (0, 1, 4, 6))
-def test_is_prime_false(n):
-    assert False
+# @pytest.mark.parametrize("n", (0, 1, 4, 6))
+# def test_is_prime_false(n):
+#     assert False
 
 
-@pytest.mark.parametrize("n", (2, 3, 5, 7))
-def test_is_prime_true(n):
-    assert False
+# @pytest.mark.parametrize("n", (2, 3, 5, 7))
+# def test_is_prime_true(n):
+#     assert False
 
 
 def test_prime_generator():
     primes = prime.prime_generator()
-    assert False
+
+    assert next(primes) == 2
+    assert next(primes) == 3
+    assert next(primes) == 5
+    assert next(primes) == 7
+
+
+def test_prime_generator__first_ten():
+    primes = prime.prime_generator()
+
+    assert list(itertools.islice(primes, 0, 10)) == [
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+    ]
 
 
 def test_prime_sieve():
-    primes = prime.prime_sieve(100)
-    assert False
+    primes = prime.prime_sieve(30)
+    assert primes == [
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+    ]
